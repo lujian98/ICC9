6620300d5d439255074f0007f612ef8fda61c0f3
lujian98 <lujian98@yahoo.com>
2020-02-11 16:47:55 -0800

    add files

---
+++ projects/icc/src/lib/icc.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'icc-test',
+  template: `
+    <p>
+      icc test 99 works!
+    </p>
+  `,
+  styles: []
+})
+export class IccComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
