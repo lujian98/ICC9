949806aaa0cf3875de1b85bec4dde3cc817c858f
lujian98 <lujian98@yahoo.com>
2020-03-26 11:16:42 -0700

    update menu button

--- projects/icc/src/lib/menu/menu.component.ts
+++ projects/icc/src/lib/menu/menu.component.ts
@@ -10,91 +10,72 @@ import { IccField } from '../items';
   styleUrls: ['./menu.component.scss']
 })
 export class IccMenuComponent implements OnChanges {
-  @Input() menuItemConfig: IccMenuItem;
-  @Output() iccMenuItemClickEvent: EventEmitter<IccMenuItem> = new EventEmitter();
-
-  menuItems: IccMenuItem = {};
   constructor(
     private itemService: IccItemFieldService,
     private fieldViewService: IccFieldViewService,
   ) { }
+  @Input() menuItemConfig: IccMenuItem;
+  @Output() iccMenuItemClickEvent: EventEmitter<IccMenuItem> = new EventEmitter();
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes.menuItemConfig) {
-      this.menuItems = {};
-      console.log( ' this.menuItemConfig=', this.menuItemConfig)
-      this.setInitialItems(this.menuItemConfig);
-      console.log(' XXXX XXX this.menuItems =', this.menuItems);
-    }
-  }
+  menuItems: IccMenuItem = {};
 
-  public setInitialItems(menuItemConfig: IccMenuItem) {
-    if (menuItemConfig) {
-      this.menuItems = this.getMenuItem(menuItemConfig);
-      if (menuItemConfig.children && menuItemConfig.children.length > 0) {
-        const items = [];
-        menuItemConfig.children.forEach((config: IccMenuItem, index) => {
-          const item = this.getMenuItem(config);
-          item.fieldView = this.fieldViewService.getFieldView(config);
-          items.push(item);
-        });
-        this.menuItems.children = items;
-      }
-    }
-  }
-
-  //           column.columnFilter = this.columnFilterService.getColumnFilterByIndex(index, columnConfigs);
-
-
-  private getMenuItem(menuItemConfig): IccField {
-    if (!menuItemConfig.type) {
-      menuItemConfig.type = 'button';
-    }
-    return this.itemService.getItem(menuItemConfig);
-  }
-
-  onMenuItemClick(menuItem: IccMenuItem) {
-    if (!menuItem.disabled) {
-      this.iccMenuItemClickEvent.emit(menuItem);
-    }
-  }
-}
-
-
-/* // test menu data
-navItems: IccMenuItem =
-{
-  title: 'AngularMix',
-  name: 'close',
-  children: [
+  testMenuItems: IccMenuItem =
     {
-      title: 'Speakers',
-      name: 'group',
+      icon: 'fas fa-ellipsis-v',
+      // name: 'close',
       children: [
         {
-          title: 'Michael Prentice',
-          name: 'person',
+          title: 'Speakers',
+          name: 'group',
+          children: [
+            {
+              title: 'Michael Prentice',
+              name: 'person',
+              children: [
+                {
+                  title: 'Delight your Organization',
+                  name: 'star_rate',
+                }
+              ]
+            },
+            {
+              title: 'Stephen Fluin',
+              name: 'person',
+              children: [
+                {
+                  title: 'What\'s up with the Web?',
+                  name: 'star_rate',
+                }
+              ]
+            },
+            {
+              title: 'Mike Brocchi',
+              name: 'person',
+              children: [
+                {
+                  title: 'My ally, the CLI',
+                  name: 'star_rate',
+                },
+                {
+                  title: 'Become an Angular Tailor',
+                  name: 'star_rate',
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: 'Sessions',
+          name: 'speaker_notes',
           children: [
             {
               title: 'Delight your Organization',
               name: 'star_rate',
-            }
-          ]
-        },
-        {
-          title: 'Stephen Fluin',
-          name: 'person',
-          children: [
+            },
             {
               title: 'What\'s up with the Web?',
               name: 'star_rate',
-            }
-          ]
-        },
-        {
-          title: 'Mike Brocchi',
-          name: 'person',
-          children: [
+            },
             {
               title: 'My ally, the CLI',
               name: 'star_rate',
@@ -104,38 +85,49 @@ navItems: IccMenuItem =
               name: 'star_rate',
             }
           ]
+        },
+        {
+          title: 'Feedback',
+          name: 'feedback',
         }
       ]
-    },
-    {
-      title: 'Sessions',
-      name: 'speaker_notes',
-      children: [
-        {
-          title: 'Delight your Organization',
-          name: 'star_rate',
-        },
-        {
-          title: 'What\'s up with the Web?',
-          name: 'star_rate',
-        },
-        {
-          title: 'My ally, the CLI',
-          name: 'star_rate',
-        },
-        {
-          title: 'Become an Angular Tailor',
-          name: 'star_rate',
-        }
-      ]
-    },
-    {
-      title: 'Feedback',
-      name: 'feedback',
+    };
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.menuItemConfig) {
+      this.menuItemConfig = this.testMenuItems;
+      this.menuItems = this.getMenuItems(this.menuItemConfig);
+      console.log(' XXXX XXX this.menuItems =', this.menuItems);
     }
-  ]
-};
+  }
 
-*/
+  private getMenuItems(configs: IccMenuItem): IccField {
+    if (configs) {
+      const menuItems: IccField = this.getMenuItem(configs);
+      if (configs.children && configs.children.length > 0) {
+        const items = [];
+        configs.children.forEach((config: IccMenuItem) => {
+          items.push(this.getMenuItems(config));
+        });
+        menuItems.children = items;
+      }
+      return menuItems;
+    }
+  }
 
+  private getMenuItem(config: IccMenuItem): IccField {
+    if (!config.type) {
+      config.type = 'button';
+    }
+    const item = this.itemService.getItem(config);
+    item.fieldView = this.fieldViewService.getFieldView(config);
+    return item;
+  }
+
+  onMenuItemClick(menuItem: IccMenuItem) {
+    if (!menuItem.disabled) {
+      this.iccMenuItemClickEvent.emit(menuItem);
+    }
+  }
+}
 
